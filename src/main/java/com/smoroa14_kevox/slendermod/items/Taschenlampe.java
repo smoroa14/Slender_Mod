d639bfdce4de7f908d7b8b28b346ac19b62681b4
smoroa14 <robertscho1240@gmail.com>
2017-10-10 16:10:55 +0200

    init

--- src/main/java/com/smoroa14_kevox/slendermod/items/Taschenlampe.java
+++ src/main/java/com/smoroa14_kevox/slendermod/items/Taschenlampe.java
@@ -1,16 +1,28 @@
 package com.smoroa14_kevox.slendermod.items;
 
+import net.minecraft.block.Block;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemDye;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemTool;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumHand;
+import net.minecraft.world.World;
+
+import java.util.Set;
 
 
-public class Taschenlampe extends ItemDye{
-    public Taschenlampe(String name)
-    {
+public class Taschenlampe extends ItemTool{
+    public Taschenlampe(ToolMaterial materialIn, Set<Block> effectiveBlocksIn, String name) {
+        super(materialIn, effectiveBlocksIn);
         this.setRegistryName(name);
         this.setUnlocalizedName(name);
         this.setCreativeTab(CreativeTabs.MISC);
     }
 
-
+    @Override
+    public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn) {
+        return super.onItemRightClick(worldIn, playerIn, handIn);
+    }
 }
