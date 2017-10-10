adf356e0f886c963803353eba7a1186d1a1d9b2a
smoroa14 <robertscho1240@gmail.com>
2017-10-10 14:59:59 +0200

    init

---
+++ src/main/java/com/smoroa14_kevox/slendermod/SlenderMod.java
@@ -0,0 +1,41 @@
+package virusmod;
+
+import com.smoroa14_kevox.slendermod.proxy.CommonProxy;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = virusmod.SlenderMod.MODID, version = virusmod.SlenderMod.VERSION)
+public class SlenderMod
+{
+    public static final String MODID = "slendermod";
+    public static final String VERSION = "1.0";
+
+    @Mod.Instance
+    public static virusmod.SlenderMod instance;
+
+    @SidedProxy(clientSide = "com.smoroa14_kevox.slendermod.proxy.ClientProxy",
+            serverSide = "com.smoroa14_kevox.slendermod.proxy.ServerProxy")
+    public static CommonProxy proxy;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        proxy.preInit(event);
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+     proxy.init(event);
+    }
+
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+        proxy.postInit(event);
+    }
+}
