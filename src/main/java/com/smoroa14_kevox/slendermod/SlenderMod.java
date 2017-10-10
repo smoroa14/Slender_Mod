ad4cbc3af940346cd4a0bb75b11528404e8c147d
smoroa14 <robertscho1240@gmail.com>
2017-10-10 15:12:50 +0200

    init

--- src/main/java/com/smoroa14_kevox/slendermod/SlenderMod.java
+++ src/main/java/com/smoroa14_kevox/slendermod/SlenderMod.java
@@ -1,21 +1,20 @@
-package virusmod;
+package com.smoroa14_kevox.slendermod;
 
-import com.smoroa14_kevox.slendermod.proxy.CommonProxy;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent
 
-@Mod(modid = virusmod.SlenderMod.MODID, version = virusmod.SlenderMod.VERSION)
+@Mod(modid = SlenderMod.MODID, version = SlenderMod.VERSION)
 public class SlenderMod
 {
     public static final String MODID = "slendermod";
     public static final String VERSION = "1.0";
 
     @Mod.Instance
-    public static virusmod.SlenderMod instance;
+    public static SlenderMod instance;
 
     @SidedProxy(clientSide = "com.smoroa14_kevox.slendermod.proxy.ClientProxy",
             serverSide = "com.smoroa14_kevox.slendermod.proxy.ServerProxy")
--- src/main/java/com/smoroa14_kevox/slendermod/proxy/CommonProxy.java
+++ src/main/java/com/smoroa14_kevox/slendermod/proxy/CommonProxy.java
@@ -1,12 +1,5 @@
-package virusmod.proxy;
+package com.smoroa14_kevox.slendermod.proxy;
 
-
-import com.smoroa14_kevox.slendermod.VirusMod;
-import com.smoroa14_kevox.slendermod.blocks.GrassVirusBlock;
-import com.smoroa14_kevox.slendermod.blocks.IceVirusBlock;
-import com.smoroa14_kevox.slendermod.blocks.WaterVirusBlock;
-import com.smoroa14_kevox.slendermod.items.VirusEssence;
-import com.smoroa14_kevox.slendermod.items.WaterVirusBucket;
 import net.minecraft.block.Block;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.init.Blocks;
@@ -25,12 +18,6 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 @Mod.EventBusSubscriber(modid = VirusMod.MODID)
 public class CommonProxy {
     //public static Block FLINT_STONE_BLOCK = new FlintstoneBlock("fredblock");
-    public static Block GRASS_VIRUS_BLOCK = new GrassVirusBlock("grassvirus");
-    public static Block ICE_VIRUS_BLOCK = new IceVirusBlock("icevirus");
-    public static Fluid WATER_VIRUS_FLUID = new WaterVirusBlock("watervirus");
-
-    public static Item WATER_VIRUS_BUCKET = new WaterVirusBucket(Blocks.WATER, "watervirusbucket");
-    public static Item VIRUS_ESSENCE = new VirusEssence("virusessence");
 
     public void init(FMLInitializationEvent event){}
     public void preInit(FMLPreInitializationEvent event){}
@@ -40,28 +27,20 @@ public class CommonProxy {
     public static void registerBlocks(RegistryEvent.Register<Block> event)
     {
         //event.getRegistry().register(FLINT_STONE_BLOCK);
-        event.getRegistry().register(GRASS_VIRUS_BLOCK);
-        event.getRegistry().register(ICE_VIRUS_BLOCK);
     }
 
     @SubscribeEvent
     public static void registerItems(RegistryEvent.Register<Item> event)
     {
         //event.getRegistry().register(new ItemBlock(FLINT_STONE_BLOCK).setRegistryName(FLINT_STONE_BLOCK.getRegistryName()));
-        event.getRegistry().register(new ItemBlock(GRASS_VIRUS_BLOCK).setRegistryName(GRASS_VIRUS_BLOCK.getRegistryName()));
-        event.getRegistry().register(new ItemBlock(ICE_VIRUS_BLOCK).setRegistryName(ICE_VIRUS_BLOCK.getRegistryName()));
-        event.getRegistry().register(VIRUS_ESSENCE);
-        event.getRegistry().register(WATER_VIRUS_BUCKET);
+        //event.getRegistry().register(WATER_VIRUS_BUCKET);
     }
 
     @SubscribeEvent
     public static void registerRenderers(ModelRegistryEvent event)
     {
         //registerRenderer(Item.getItemFromBlock(FLINT_STONE_BLOCK));
-        registerRenderer(Item.getItemFromBlock(ICE_VIRUS_BLOCK));
-        registerRenderer(Item.getItemFromBlock(GRASS_VIRUS_BLOCK));
-        registerRenderer(VIRUS_ESSENCE);
-        registerRenderer(WATER_VIRUS_BUCKET);
+        //registerRenderer(WATER_VIRUS_BUCKET);
     }
 
     private static void registerRenderer(Item item)
