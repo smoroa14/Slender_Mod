46ded1b71ef847a78e7feb2681ad40078a6478a0
smoroa14 <robertscho1240@gmail.com>
2017-10-11 08:25:33 +0200

    init

--- src/main/java/com/smoroa14_kevox/slendermod/proxy/ClientProxy.java
+++ src/main/java/com/smoroa14_kevox/slendermod/proxy/ClientProxy.java
@@ -1,7 +1,11 @@
 package com.smoroa14_kevox.slendermod.proxy;
 
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.relauncher.Side;
 
 @Mod.EventBusSubscriber(Side.CLIENT)
@@ -9,11 +13,7 @@ public class ClientProxy extends CommonProxy {
     @Override
     public void preInit(FMLPreInitializationEvent event) {
         super.preInit(event);
-        /*GameRegistry.addShapedRecipe(new ResourceLocation("Virus Essence"), null, new ItemStack(CommonProxy.VIRUS_ESSENCE),
-                "XXX",
-                "XOX",
-                "XXX",
-                'X', Items.SLIME_BALL,
-                'O', Items.NETHER_STAR);*/
+        /*GameRegistry.addShapelessRecipe(new ResourceLocation("Virus Essence"), null, new ItemStack(CommonProxy.TASCHENLAMPE),
+                new Ingredient(new ItemStack(CommonProxy.TASCHENLAMPE)), new Ingredient(new ItemStack(CommonProxy.BATTERIE));*/
     }
 }
