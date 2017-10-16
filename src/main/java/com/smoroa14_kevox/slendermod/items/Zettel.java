21e9e855c92ea24ec875681bcc1d91d1b11cbfc3
Kevin Albrich <kevin.albrich@gmx.at>
2017-10-16 10:54:21 +0200

    idk

--- src/main/java/com/smoroa14_kevox/slendermod/items/Zettel.java
+++ src/main/java/com/smoroa14_kevox/slendermod/items/Zettel.java
@@ -1,11 +1,22 @@
 package com.smoroa14_kevox.slendermod.items;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumHand;
+import net.minecraft.world.World;
 
 public class Zettel extends Item{
     public Zettel(String name) {
         this.setRegistryName(name);
         this.setUnlocalizedName(name);
         this.setCreativeTab(CreativeTabs.MISC);
+    }
+
+    @Override
+    public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn) {
+
+    return super.onItemRightClick(worldIn, playerIn, handIn);
     }
 }
