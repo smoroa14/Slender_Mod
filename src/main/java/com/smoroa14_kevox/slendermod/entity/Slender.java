0d002514f08a70fda29aeca8c011d8dc937a379a
smoroa14 <robertscho1240@gmail.com>
2017-12-21 11:26:38 +0100

    am slenderman rumgebastelt

--- src/main/java/com/smoroa14_kevox/slendermod/entity/Slender.java
+++ src/main/java/com/smoroa14_kevox/slendermod/entity/Slender.java
@@ -2,52 +2,25 @@ package com.smoroa14_kevox.slendermod.entity;
 
 import com.google.common.base.Function;
 import com.google.common.base.Predicate;
-import com.smoroa14_kevox.slendermod.SlenderMod;
-import com.smoroa14_kevox.slendermod.proxy.CommonProxy;
-import net.minecraft.block.Block;
 import net.minecraft.entity.*;
 import net.minecraft.entity.ai.*;
-import net.minecraft.entity.ai.attributes.AttributeModifier;
-import net.minecraft.entity.ai.attributes.IAttribute;
-import net.minecraft.entity.ai.attributes.IAttributeInstance;
-import net.minecraft.entity.ai.attributes.RangedAttribute;
 import net.minecraft.entity.monster.*;
-import net.minecraft.entity.passive.EntityChicken;
-import net.minecraft.entity.passive.EntityVillager;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
-import net.minecraft.init.Items;
 import net.minecraft.init.SoundEvents;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.network.datasync.DataParameter;
-import net.minecraft.network.datasync.DataSerializers;
-import net.minecraft.network.datasync.EntityDataManager;
-import net.minecraft.pathfinding.PathNavigateGround;
 import net.minecraft.pathfinding.PathNodeType;
 import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.DamageSource;
-import net.minecraft.util.EntitySelectors;
-import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.SoundEvent;
-import net.minecraft.util.datafix.DataFixer;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
-import net.minecraft.world.DifficultyInstance;
-import net.minecraft.world.EnumDifficulty;
 import net.minecraft.world.World;
-import net.minecraft.world.storage.loot.LootTableList;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 
 import javax.annotation.Nullable;
-import java.util.Calendar;
 import java.util.List;
-import java.util.UUID;
 
 public class Slender extends EntityMob {
 
@@ -102,7 +75,7 @@ public class Slender extends EntityMob {
 
 
     /**
-     * Teleport the enderman to a random nearby position
+     * Teleport the slenderman to a random nearby position
      */
     protected boolean teleportRandomly() {
         System.out.println("-------------------------------------------------------------------------------------------------------- ttr");
@@ -187,7 +160,8 @@ public class Slender extends EntityMob {
     }
 
     static class AIFindPlayer extends EntityAINearestAttackableTarget<EntityPlayer> {
-        private final Slender enderman;
+        private final Slender slenderman
+                ;
         /**
          * The player
          */
@@ -197,7 +171,7 @@ public class Slender extends EntityMob {
 
         public AIFindPlayer(Slender p_i45842_1_) {
             super(p_i45842_1_, EntityPlayer.class, false);
-            this.enderman = p_i45842_1_;
+            this.slenderman = p_i45842_1_;
         }
 
         /**
@@ -205,9 +179,9 @@ public class Slender extends EntityMob {
          */
         public boolean shouldExecute() {
             double d0 = this.getTargetDistance();
-            this.player = this.enderman.world.getNearestAttackablePlayer(this.enderman.posX, this.enderman.posY, this.enderman.posZ, d0, d0, (Function) null, new Predicate<EntityPlayer>() {
+            this.player = this.slenderman.world.getNearestAttackablePlayer(this.slenderman.posX, this.slenderman.posY, this.slenderman.posZ, d0, d0, (Function) null, new Predicate<EntityPlayer>() {
                 public boolean apply(@Nullable EntityPlayer p_apply_1_) {
-                    return p_apply_1_ != null && Slender.AIFindPlayer.this.enderman.shouldAttackPlayer(p_apply_1_);
+                    return p_apply_1_ != null && Slender.AIFindPlayer.this.slenderman.shouldAttackPlayer(p_apply_1_);
                 }
             });
             return this.player != null;
@@ -234,10 +208,10 @@ public class Slender extends EntityMob {
          */
         public boolean shouldContinueExecuting() {
             if (this.player != null) {
-                if (!this.enderman.shouldAttackPlayer(this.player)) {
+                if (!this.slenderman.shouldAttackPlayer(this.player)) {
                     return false;
                 } else {
-                    this.enderman.faceEntity(this.player, 10.0F, 10.0F);
+                    this.slenderman.faceEntity(this.player, 10.0F, 10.0F);
                     return true;
                 }
             } else {
@@ -257,13 +231,13 @@ public class Slender extends EntityMob {
                 }
             } else {
                 if (this.targetEntity != null) {
-                    if (this.enderman.shouldAttackPlayer((EntityPlayer) this.targetEntity)) {
-                        if (((EntityPlayer) this.targetEntity).getDistanceSqToEntity(this.enderman) < 16.0D) {
-                            this.enderman.teleportRandomly();
+                    if (this.slenderman.shouldAttackPlayer((EntityPlayer) this.targetEntity)) {
+                        if (((EntityPlayer) this.targetEntity).getDistanceSqToEntity(this.slenderman) < 16.0D) {
+                            this.slenderman.teleportRandomly();
                         }
 
                         this.teleportTime = 0;
-                    } else if (((EntityPlayer) this.targetEntity).getDistanceSqToEntity(this.enderman) > 256.0D && this.teleportTime++ >= 30 && this.enderman.teleportToEntity(this.targetEntity)) {
+                    } else if (((EntityPlayer) this.targetEntity).getDistanceSqToEntity(this.slenderman) > 256.0D && this.teleportTime++ >= 30 && this.slenderman.teleportToEntity(this.targetEntity)) {
                         this.teleportTime = 0;
                     }
                 }
@@ -326,13 +300,11 @@ public class Slender extends EntityMob {
             double x = player.posX - this.posX;
             double y = player.posY - this.posY;
             double z = player.posZ - this.posZ;
-            System.out.println(x + " - " + y + " - " + z);
             x = x<0?x*(-1):x;
             y = y<0?y*(-1):y;
             z = z<0?z*(-1):z;
-            System.out.println(x + " | " + y + " | " + z);
 
-            if(x < 10 && y < 10 && z < 10)player.addPotionEffect(new PotionEffect(Potion.getPotionById(15), 10, 1));
+            if(x < 10 && y < 10 && z < 10)player.addPotionEffect(new PotionEffect(Potion.getPotionById(15), 12, 155));
         }
 
         super.onUpdate();
